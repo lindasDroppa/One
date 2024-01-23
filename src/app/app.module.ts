94c94f874241f3b848a73e6fc1bf81ab2f2ccff4
lindasDroppa <139218024+lindasDroppa@users.noreply.github.com>
2024-01-23 19:10:27 +0200

    User login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { LoginUserComponent } from './login-user/login-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    LoginUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
